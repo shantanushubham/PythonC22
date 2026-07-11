c9c2cab4d57a9fa416dbf74b98a42510a9d6f4bf
Shantanu Shubham <shantanu0707@gmail.com>
2026-07-11 19:12:00 +0530

    Initial Commit - Python C22

---
+++ test.py
@@ -0,0 +1,35 @@
+# Programming = Giving instructions to a computer to do a task.
+# A programming language is a language which can be used to write programs.
+
+# Python -> compiler -> Machine Language
+
+# In Programming Languages, we have
+# 1. Statement
+# 2. Expression
+# 3. DataType
+
+# 1 byte = 8 bits | 1 bit = []
+
+# [][][][][][][][] -> 2^8 = 256 /2 = 128 | -128 to 0 to 127
+
+# Primitive DataTypes
+# 1. Boolean (True/False) [] -> 1 bit
+# 2. Byte (stores numbers) | -128 to 127 -> 8 bits -> 1 byte
+# 3. Char aka Character (it stores symbols like 'a', 'b', '&') [same as byte] -> 1 byte
+# 4. Short (stores numbers) | -32,768 to 32,767 -> 16 bits -> 2 bytes
+# 5. Int (stores numbers) | -2,147,483,648 to 2,147,483,647 -> 32 bits -> 4 bytes
+# 6. Long (stores numbers) | $-2^{63}$ to $2^{63} - 1$ -> 64 bit -> 8 bytes
+# 7. Float (stores numbers with decimal) | +-(1.2) \times 10^{-38}$ to $\pm3.4 \times 10^{38}$ (6-7 decimal digits of precision) -> 32 bits -> 4 bytes
+# 8. Double (stores numbers with decimal) | $\pm2.3 \times 10^{-308}$ to $\pm1.7 \times 10^{308}$ (15 decimal digits of precision) -> 64 bits -> 8 bytes
+
+
+# Non-Primitive DataTypes
+# 1. String (Used to represent words and sentences). Eg: "My name is Shantanu." -> 20 bytes
+
+my_var = 10
+my_boolean_var = True
+my_name = "Shantanu"
+my_char = 'c'
+
+sum = 9223372036854775807 + 10
+print()
